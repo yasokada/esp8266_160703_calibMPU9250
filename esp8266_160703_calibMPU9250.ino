5fbc1f31107c9325d809276f66586b3ab6e90c8c
Yasuhiko OKADA <yasokada@gmail.com>
2016-07-03 09:33:04 +0900

    use Interrupt / remove Calibartion()

--- esp8266_160703_calibMPU9250.ino
+++ esp8266_160703_calibMPU9250.ino
@@ -1,5 +1,7 @@
 /*
  * v0.1 2016 Jul. 03
+ *   - remove Calibration()
+ *   - use Interrupt
  *   - import for MPU-9250
  *   - based on the code at
  *   http://www.i2cdevlib.com/forums/topic/96-arduino-sketch-to-automatically-calculate-mpu6050-offsets/
@@ -11,19 +13,27 @@
 #include <ESP8266WiFi.h>
 MPU9150 accelgyro;
 
-///////////////////////////////////   CONFIGURATION   /////////////////////////////
-//Change this 3 variables if you want to fine tune the skecth to your needs.
-int buffersize=1000;     //Amount of readings used to average, make it higher to get more precision but sketch will be slower  (default:1000)
-int acel_deadzone=8;     //Acelerometer error allowed, make it lower to get more precision, but sketch may not converge  (default:8)
-int giro_deadzone=1;     //Giro error allowed, make it lower to get more precision, but sketch may not converge  (default:1)
+#define INTERRUPT_PIN 14  // ESP8266
 
+int buffersize = 1000;     //Amount of readings used to average, make it higher to get more precision but sketch will be slower  (default:1000)
 int16_t ax, ay, az,gx, gy, gz;
 
-int mean_ax,mean_ay,mean_az,mean_gx,mean_gy,mean_gz,state=0;
-int ax_offset,ay_offset,az_offset,gx_offset,gy_offset,gz_offset;
+int mean_ax, mean_ay, mean_az;
+int mean_gx, mean_gy, mean_gz;
+int state=0;
+int ax_offset, ay_offset, az_offset;
+int gx_offset, gy_offset, gz_offset;
+
+uint16_t packetSize;
+uint16_t fifoCount;
 
 void meansensors();
-void calibration();
+void showData();
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
 
 void setup() {
   WiFi.disconnect();
@@ -32,6 +42,7 @@ void setup() {
   Serial.begin(115200);
 
   accelgyro.initialize();
+  pinMode(INTERRUPT_PIN, INPUT);
 
   while (Serial.available() && Serial.read()); // empty buffer
   while (!Serial.available()){
@@ -40,21 +51,13 @@ void setup() {
   }                
   while (Serial.available() && Serial.read()); // empty buffer again
 
-  // start message
-  Serial.println("\nMPU6050 Calibration Sketch");
-  delay(2000);
-  Serial.println("\nYour MPU6050 should be placed in horizontal position, with package letters facing up. \nDon't touch it until you see a finish message.\n");
+  packetSize = accelgyro.dmpGetFIFOPacketSize();
+
+  Serial.println("\nYour MPU-9250 should be placed in horizontal position, with package letters facing up. \nDon't touch it until you see a finish message.\n");
   delay(3000);
-  // verify connection
-  Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
-  delay(1000);
-  // reset offsets
-  accelgyro.setXAccelOffset(0);
-  accelgyro.setYAccelOffset(0);
-  accelgyro.setZAccelOffset(0);
-  accelgyro.setXGyroOffset(0);
-  accelgyro.setYGyroOffset(0);
-  accelgyro.setZGyroOffset(0);  
+//  // verify connection
+//  Serial.println(accelgyro.testConnection() ? "MPU9250 connection successful" : "MPU9250 connection failed");
+//  delay(1000);
 }
 
 void loop() {
@@ -66,16 +69,15 @@ void loop() {
   }
 
   if (state==1) {
-    Serial.println("\nCalculating offsets...");
-    calibration();
+    Serial.println("\nMeasuring...");
+    showData(); 
     state++;
     delay(1000);
   }
 
   if (state==2) {
     meansensors();
-    Serial.println("\nFINISHED!");
-    Serial.print("\nSensor readings with offsets:\t");
+    Serial.print("\nSensor readings with zero offsets:\t");
     Serial.print(mean_ax); 
     Serial.print("\t");
     Serial.print(mean_ay); 
@@ -87,104 +89,104 @@ void loop() {
     Serial.print(mean_gy); 
     Serial.print("\t");
     Serial.println(mean_gz);
-    Serial.print("Your offsets:\t");
-    Serial.print(ax_offset); 
-    Serial.print("\t");
-    Serial.print(ay_offset); 
-    Serial.print("\t");
-    Serial.print(az_offset); 
-    Serial.print("\t");
-    Serial.print(gx_offset); 
-    Serial.print("\t");
-    Serial.print(gy_offset); 
-    Serial.print("\t");
-    Serial.println(gz_offset); 
     Serial.println("\nData is printed as: acelX acelY acelZ giroX giroY giroZ");
     Serial.println("Check that your sensor readings are close to 0 0 16384 0 0 0");
-    Serial.println("If calibration was succesful write down your offsets so you can set them in your projects using something similar to mpu.setXAccelOffset(youroffset)");
-    while (1);
+    state++;
   }
 }
 
 ///////////////////////////////////   FUNCTIONS   ////////////////////////////////////
 void meansensors(){
-  long i=0,buff_ax=0,buff_ay=0,buff_az=0,buff_gx=0,buff_gy=0,buff_gz=0;
+  long i=0;
+  int buff_ax=0;
+  int buff_ay=0;
+  int buff_az=0;
+  int buff_gx=0;
+  int buff_gy=0;
+  int buff_gz=0;
 
-  while (i<(buffersize+101)){
-    // read raw accel/gyro measurements from device
-    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-
-#if 0 // for debug
-    Serial.print(F("ax="));
-    Serial.print(ax);
-    Serial.print(F(" ,ay="));
-    Serial.print(ay);
-    Serial.print(F(" ,az="));
-    Serial.print(az);
-    Serial.println();    
-#endif
-    
-    if (i>100 && i<=(buffersize+100)){ //First 100 measures are discarded
-      buff_ax=buff_ax+ax;
-      buff_ay=buff_ay+ay;
-      buff_az=buff_az+az;
-      buff_gx=buff_gx+gx;
-      buff_gy=buff_gy+gy;
-      buff_gz=buff_gz+gz;
+  while (i < (buffersize + 101) ){
+    fifoCount = accelgyro.getFIFOCount();
+    if (!mpuInterrupt && fifoCount < packetSize) {
+      continue;
     }
-    if (i==(buffersize+100)){
-      mean_ax=buff_ax/buffersize;
-      mean_ay=buff_ay/buffersize;
-      mean_az=buff_az/buffersize;
-      mean_gx=buff_gx/buffersize;
-      mean_gy=buff_gy/buffersize;
-      mean_gz=buff_gz/buffersize;
+    mpuInterrupt = false;
+    
+    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+        
+    if ( (i > 100) && i <= (buffersize + 100) ){ // First 100 discarded
+      buff_ax = buff_ax + ax;
+      buff_ay = buff_ay + ay;
+      buff_az = buff_az + az;
+      buff_gx = buff_gx + gx;
+      buff_gy = buff_gy + gy;
+      buff_gz = buff_gz + gz;
+    }
+    if ( i == (buffersize + 100) ) {
+      mean_ax = buff_ax / buffersize;
+      mean_ay = buff_ay / buffersize;
+      mean_az = buff_az / buffersize;
+      mean_gx = buff_gx / buffersize;
+      mean_gy = buff_gy / buffersize;
+      mean_gz = buff_gz / buffersize;
     }
     i++;
-    delay(2); //Needed so we don't get repeated measures
   }
 }
 
-void calibration(){
-  ax_offset=-mean_ax/8;
-  ay_offset=-mean_ay/8;
-  az_offset=(16384-mean_az)/8;
+void showData()
+{
+  accelgyro.setXAccelOffset(0);
+  accelgyro.setYAccelOffset(0);
+  accelgyro.setZAccelOffset(0);
+  accelgyro.setXGyroOffset(0);
+  accelgyro.setYGyroOffset(0);
+  accelgyro.setZGyroOffset(0);
 
-  gx_offset=-mean_gx/4;
-  gy_offset=-mean_gy/4;
-  gz_offset=-mean_gz/4;
-  while (1){
-    int ready=0;
-    accelgyro.setXAccelOffset(ax_offset);
-    accelgyro.setYAccelOffset(ay_offset);
-    accelgyro.setZAccelOffset(az_offset);
-
-    accelgyro.setXGyroOffset(gx_offset);
-    accelgyro.setYGyroOffset(gy_offset);
-    accelgyro.setZGyroOffset(gz_offset);
-
+  for(int loop = 0; loop < 10; loop++) {
     meansensors();
-    Serial.println("...");
-
-    if (abs(mean_ax)<=acel_deadzone) ready++;
-    else ax_offset=ax_offset-mean_ax/acel_deadzone;
-
-    if (abs(mean_ay)<=acel_deadzone) ready++;
-    else ay_offset=ay_offset-mean_ay/acel_deadzone;
-
-    if (abs(16384-mean_az)<=acel_deadzone) ready++;
-    else az_offset=az_offset+(16384-mean_az)/acel_deadzone;
-
-    if (abs(mean_gx)<=giro_deadzone) ready++;
-    else gx_offset=gx_offset-mean_gx/(giro_deadzone+1);
-
-    if (abs(mean_gy)<=giro_deadzone) ready++;
-    else gy_offset=gy_offset-mean_gy/(giro_deadzone+1);
-
-    if (abs(mean_gz)<=giro_deadzone) ready++;
-    else gz_offset=gz_offset-mean_gz/(giro_deadzone+1);
-
-    if (ready==6) break;
+    Serial.print(" ,mean_ax:");
+    Serial.print(mean_ax);
+    Serial.print(" ,mean_ay:");
+    Serial.print(mean_ay);
+    Serial.print(" ,mean_az:");
+    Serial.print(mean_az);
+    //
+    Serial.print(" ,mean_gx:");
+    Serial.print(mean_gx);
+    Serial.print(" ,mean_gy:");
+    Serial.print(mean_gy);
+    Serial.print(" ,mean_gz:");
+    Serial.print(mean_gz);
+    //
+    Serial.println();
   }
+
+  accelgyro.setXAccelOffset(-81/8);
+  accelgyro.setYAccelOffset(-264/8);
+  accelgyro.setZAccelOffset((16384-16308)/8);
+  accelgyro.setXGyroOffset(0);
+  accelgyro.setYGyroOffset(0);
+  accelgyro.setZGyroOffset(0);
+
+  for(int loop = 0; loop < 10; loop++) {
+    meansensors();
+    Serial.print(" ,mean_ax:");
+    Serial.print(mean_ax);
+    Serial.print(" ,mean_ay:");
+    Serial.print(mean_ay);
+    Serial.print(" ,mean_az:");
+    Serial.print(mean_az);
+    //
+    Serial.print(" ,mean_gx:");
+    Serial.print(mean_gx);
+    Serial.print(" ,mean_gy:");
+    Serial.print(mean_gy);
+    Serial.print(" ,mean_gz:");
+    Serial.print(mean_gz);
+    //
+    Serial.println();
+  }
+  
 }
 
